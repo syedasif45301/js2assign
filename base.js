aaf6fb04e579897be357651326882fea63372821
syed asif <syedasif45301@gmail.com>
2023-10-19 21:51:09 +0530

    msg

---
+++ base.js
@@ -0,0 +1,36 @@
+var sen="Hello Everyone! Welcome to javascript"
+
+console.log(sen.toUpperCase())
+console.log(sen.toLowerCase())
+
+
+console.log("Hello World".substr(0,5))
+
+var str="Facebook,Google,Microsoft,Apple,IBM,Oracle,Amazon"
+console.log(str)
+console.log(str.split(","))
+
+var j="javascript"
+console.log(j.indexOf("a"))
+console.log(j.lastIndexOf("a"))
+
+var str1="Javascript is used "
+var str2="to add functionalities to the web app"
+var str3=str1+str2
+console.log(str3)
+
+var s="This course is all about Javascript"
+console.log(s.includes("Javascript"))
+
+console.log("generating random number using math")
+console.log(Math.ceil(Math.random()*100))
+console.log(Math.ceil(Math.random()*50+50))
+
+
+console.log("1 1 1 1 1\n2 1 2 4 8\n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125")
+
+var str="He earns 50000 from salary per month, 10000 annual bonus"
+console.log(str.substr(9,5)*12+parseInt(str.substr(38,5)))
+
+
+
